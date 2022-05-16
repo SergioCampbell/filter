05bfe3c14cef2161f9d70f28805da7ff5ae84958
Sergio Campbell <info.sergiocampbell@gmail.com>
2022-05-16 17:53:22 -0500

    cleaning

--- src/components/AllPlayers.tsx
+++ src/components/AllPlayers.tsx
@@ -1,40 +1,11 @@
-import { useState } from 'react'
 import Filter from './Filter/Filter'
-import { InputText } from './Filter/InputText';
 
 export default function AllPlayers () {
-    const [tableRowsData, setTableRowsData] = useState([]);
-
-  /* const onChange = async (e: any) => {
-    let searchData = Data.filter((item: any) => {
-      if (
-        item.first_name
-          .toString()
-          .toLowerCase()
-          .includes(e.target.value.toLowerCase())
-      ) {
-        return item;
-      }
-    });
-    setTableRowsData(searchData);
-  }; */
-
     return (
         <>
           <h1>All players are here</h1>
 
-            {/* <InputText
-            type="search"
-            onInput={(e: any) => setGlobalFilter(e.target.value)}
-            placeholder="Buscar..."
-            width="20%"
-          /> */}
-
           <Filter />
-
-            <ol>
-                {/*players.map((item: any, i: number) => <li key={i}>{item.first_name}</li>)*/}
-            </ol>
         </>
     )
 }
--- src/data.ts
+++ src/data.ts
@@ -1,75 +1,30 @@
-import { useEffect, useState } from "react";
-
-  /* const [players, setPlayers] = useState([
-      { first_name: "", last_name: "", h_in: 0, h_meters: 0 },
-    ]);
-  const API = "https://mach-eight.uc.r.appspot.com";
-
-  const getApi = (url: string) => {
-    fetch(API)
-    .then((res) => res.json())
-    .then((data) => console.log(data))
-  }
-
-  useEffect(() => {
-    getApi(API)
-  }, []);
-
-  export { getApi } */
-
-
- const Data = async () => {
+const Data = async () => {
    const API = "https://mach-eight.uc.r.appspot.com";
-   //const [players, setPlayers] = useState([{first_name: '', last_name: '', h_in: 0, h_meters: 0}]);
 
    const players: any = [];
-
-   /* const getAll = async () => {
-     try {
-      const response = await fetch(API);
-      const all = await response.json();
-      console.log('All \n',all);
-      
-      players.push(all.values);
-     } catch (error) {
-       console.log('There are some erros here: ', error);       
-     }
-   }; */
 
    try {
     fetch(API)
     .then((response) => response.json())
     .then((data) => players.push(data.values))
-    //console.log('@@@@@@@@@@@@@',players.pop(2));
+    //console.log('@@@@@@@@@@@@@',players);
    } catch (error) {
     console.error('There are some erros here: ', error);
    }
 
-   /* useEffect(() => {
-     try {
-      fetch(API)
-      .then((response) => response.json())
-      .then((data) => console.log(data.values)
-      );
-     } catch (error) {
-      console.log('There are some erros here: ', error);
-     }
-   }, []); */
-
    const data = [
-     /* {
+     {
        first_name: 'Fuancho',
        last_name: 'Algo',
        h_in: 1.4,
        h_meters: 11,
-     }, */
+     },
      /* {
        first_name:players.map((item: any) => item.first_name),
        last_name: players.map((item: any) => item.last_name),
        h_in: players.map((item: any) => item.h_in),
        h_meters: players.map((item: any) => item.h_meters),
      }, */
-     {players}
    ];
  };
 
