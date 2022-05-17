3d00e29b57a98c329dfb192dbc94277869e8316c
Sergio Campbell <info.sergiocampbell@gmail.com>
2022-05-17 17:26:07 -0500

    little changes

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Filter from './components/Filter/Filter';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+it('get response from API', () => {
+  const api = render.caller(
+    <Filter />
+  )
+})
